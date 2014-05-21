bcb96c5d0a097de49c359a695822ab3fe7644afb
Graeme Lennon <graeme.lennon@gmail.com>
2014-05-20 22:24:30 -0400

    Some new stuff.

---
+++ Assets/Editor/ManagePlayerPrefs.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public static class ManagePlayerPrefs {
+
+	[MenuItem("Edit/Player Preferences/Clear All")]
+	static void ClearMyPlayerPrefs()
+	{
+		PlayerPrefs.DeleteAll();
+		PlayerPrefs.Save();
+	}
+}
